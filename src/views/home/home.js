5c74de775aea0247f0d075092a5052cb4c72ea15
JamylSarmiento <jamylsarmientomunoa@gmail.com>
2024-06-30 02:06:57 -0500

    first commit

---
+++ src/views/home/home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hola juanito comeme un wevo</Text>
+      <Button title="Reservar cita" onPress={() => console.log("Reservar cita")} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
---
+++ src/views/login/login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
+import {Home} from "../home/home"
+
+const Login = () => {
+  const [dni, setDni] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica para validar el inicio de sesión
+    if (dni === 'usuario' && password === 'contraseña') {
+      Alert.alert('¡Inicio de sesión exitoso!');
+      navigation.navigate('Home'); // Navegar a la pantalla Home
+    } else {
+      Alert.alert('Error', 'DNI o contraseña incorrectos');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="DNI"
+          placeholderTextColor="#FFFFFF"
+          onChangeText={text => setDni(text)}
+          value={dni}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          placeholderTextColor="#FFFFFF"
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+          value={password}
+        />
+        <Button
+          title="Iniciar Sesión"
+          onPress={handleLogin}
+          color="#40E0D0"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003366',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
+    width: '80%',
+    maxWidth: 300,
+  },
+  input: {
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+    borderRadius: 5,
+    color: '#FFFFFF',
+  },
+});
+
+export default Login;
